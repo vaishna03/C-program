c0c7421ebf0c67001c00c83175194426a7d9abac
vaishna <113762032+vaishna03@users.noreply.github.com>
2024-08-14 02:28:32 -0700

    Add files via upload

---
+++ hollowtriangleleft.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    int i,j;
+    printf("enter n :");
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            if(j==1||j==i||i==n){
+                printf("*");
+
+            }
+            else{
+                printf(" ");
+            }
+        }
+    printf("\n");
+    }
+    return 0;
+}
